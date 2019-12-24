e74b5294c5121d08e9cb27403b032b525421c290
rsbear <hellorosss@gmail.com>
2019-12-24 16:04:57 -0600

    switch from yarn to npm

--- dist/sendRefreshToken.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendRefreshToken = (res, token) => {
-    res.cookie("rfs", token, {
-        httpOnly: true,
-    });
-};
-//# sourceMappingURL=sendRefreshToken.js.map
