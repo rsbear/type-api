f16499079cff9cbd30e6f730c06add260368b012
rsbear <hellorosss@gmail.com>
2020-02-13 11:52:26 -0600

    I'm not sure why cookies arent being made for production version

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -1,7 +1,8 @@
 import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie("rfs", token, {
+  const test = res.cookie("rfs", token, {
     httpOnly: true,
   });
+  console.log(test)
 };
