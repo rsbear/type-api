54c191749e22967ed474846d1b9f3722288e9a83
rsbear <hellorosss@gmail.com>
2020-02-13 13:06:35 -0600

    Heroku sets a rule that doesn't permit setting cookies across domains

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("rfs", token, {
-    // domain: process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://typefeel.com",
+    domain: process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://typefeel.com",
     httpOnly: true,
   });
 };
