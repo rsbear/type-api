8d65aa3d9ada668f711a7c12f4dde30b433fc7b4
rsbear <hellorosss@gmail.com>
2020-02-25 11:16:28 -0600

    remove domain and path on cookie

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -2,8 +2,7 @@ import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("rfs", token, {
-    domain: process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://typefeel.com",
-    httpOnly: true,
+    httpOnly: false,
     path: '/'
   });
 };
