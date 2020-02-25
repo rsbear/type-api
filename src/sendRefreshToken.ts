22a44ea65cd84e0f7644df4dd4eb0e7cb45c320e
rsbear <hellorosss@gmail.com>
2020-02-25 14:36:31 -0600

    ....

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("rfs", token, {
+    domain: process.env.NODE_ENV !== 'production' ? 'localhost' : 'typefeel.com',
     httpOnly: true,
     path: '/'
   });
