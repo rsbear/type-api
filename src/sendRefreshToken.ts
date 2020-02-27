7832583b888327befb0ff44942974f675e74c866
rsbear <hellorosss@gmail.com>
2020-02-26 19:57:15 -0600

    secure cookie

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("rfs", token, {
-    domain: process.env.NODE_ENV !== 'production' ? 'localhost' : 'typefeel-server.herokuapp.com' || 'typefeel.com',
+    domain: process.env.NODE_ENV !== 'production' ? 'localhost' : 'typefeel.com',
     httpOnly: true,
     sameSite: 'none',
     secure: process.env.NODE_ENV !== 'production' ? false : true,
