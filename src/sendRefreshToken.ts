3af25cfd9968a7d1018d30802dc185e1a9a52181
rsbear <hellorosss@gmail.com>
2020-02-27 15:25:33 -0600

    yes

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -1,9 +1,9 @@
 import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie("refresh", token, {
-    // domain:
-    //  process.env.NODE_ENV !== "production" ? "localhost" : ".typefeel.com",
+  res.cookie("typefeel_sesh", token, {
+    domain:
+      process.env.NODE_ENV !== "production" ? "localhost" : ".typefeel.com",
     httpOnly: true,
     sameSite: "none",
     secure: process.env.NODE_ENV !== "production" ? false : true
