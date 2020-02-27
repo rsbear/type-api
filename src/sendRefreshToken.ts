8384a145398a0d91f2bf816f13d21a1fd1e308af
rsbear <hellorosss@gmail.com>
2020-02-27 03:44:42 -0600

    yes

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -1,13 +1,11 @@
 import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res
-    .cookie("refresh", token, {
-      // domain:
-      //  process.env.NODE_ENV !== "production" ? "localhost" : "typefeel.com",
-      httpOnly: true,
-      sameSite: "none",
-      secure: process.env.NODE_ENV !== "production" ? false : true
-    })
-    .send();
+  res.cookie("refresh", token, {
+    // domain:
+    //  process.env.NODE_ENV !== "production" ? "localhost" : "typefeel.com",
+    httpOnly: true,
+    sameSite: "none",
+    secure: process.env.NODE_ENV !== "production" ? false : true
+  });
 };
