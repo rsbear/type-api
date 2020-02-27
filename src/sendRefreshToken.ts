e65714eafddc2e9543ce0ec7cedbaf68915cd325
rsbear <hellorosss@gmail.com>
2020-02-26 19:47:27 -0600

    secure cookie

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -5,6 +5,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
     // domain: process.env.NODE_ENV !== 'production' ? 'localhost' : 'typefeel-server.herokuapp.com' || 'typefeel.com',
     httpOnly: true,
     sameSite: 'none',
+    secure: process.env.NODE_ENV !== 'production' ? false : true,
     path: '/'
   });
 };
