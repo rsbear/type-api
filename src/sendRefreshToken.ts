3c0c773d077186dcc752243c36e4a33dc4259876
rsbear <hellorosss@gmail.com>
2020-02-26 19:38:15 -0600

    sameSite to none on cookie

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -4,6 +4,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("rfs", token, {
     // domain: process.env.NODE_ENV !== 'production' ? 'localhost' : 'typefeel-server.herokuapp.com' || 'typefeel.com',
     httpOnly: true,
+    sameSite: 'none',
     path: '/'
   });
 };
@@ -11,6 +12,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
 export const sendRefreshToken2 = (res: Response, token: string) => {
   res.cookie("rfs", token, {
     domain: 'typefeel.com',
+    sameSite: 'none',
     httpOnly: true,
     path: '/'
   });
